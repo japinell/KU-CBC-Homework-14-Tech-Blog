c459146e49bd60849e0aab14b21830f39ae8d009
JAPinell <japinell@yahoo.com>
2021-06-19 15:08:40 -0500

    Added seed data files

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,21 +1,21 @@
 const sequelize = require('../config/connection');
 const { User, Project } = require('../models');
 
-const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const users = await User.bulkCreate(userData, {
+  const posts = await Post.bulkCreate(postData, {
     individualHooks: true,
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment,
+      post_id: posts[Math.floor(Math.random() * users.length)].id,
     });
   }
 
