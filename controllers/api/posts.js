d0c9bfaebb075fc5804638c2fd84748a270ee838
JAPinell <japinell@yahoo.com>
2021-06-19 15:44:47 -0500

    Cleaned up the Post route

--- controllers/api/posts.js
+++ controllers/api/posts.js
@@ -1,60 +1,128 @@
+// 
+// Handles CRUD operations for Post model
+// 
 const router = require('express').Router();
-const { User } = require('../../models');
+const { post } = require('../../models');
 
+// Get all posts - Data will be in the res.body
+router.get('/', async (req, res) => {
+  try {
+    // Get all posts with their related data
+    const postData = await Post.findAll({
+      include: [{
+        model: post,
+        attributes: {
+          exclude: ['password']
+        }
+      }, {
+        model: Comment
+      }],
+    });
+
+    // Serialize data so the template can read it
+    const posts = postData.map((post) => post.get({
+      plain: true
+    }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', {
+      posts,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Get a post by id - Data will be in the res.body
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await post.findByPk(req.params.id, {
+      include: [{
+        model: post,
+        attributes: {
+          exclude: ['password']
+        }
+      }, {
+        model: Comment
+      }],
+    });
+
+    if (!postData) {
+      res.status(404).json({
+        message: 'No post found with this id!'
+      });
+      return;
+    }
+
+    const post = postData.get({
+      plain: true
+    });
+
+    res.render('post', {
+      ...post,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Post a post - Data is in the req.body and req.session
 router.post('/', async (req, res) => {
   try {
-    const userData = await User.create(req.body);
+    const postData = await Post.create(req.body);
 
     req.session.save(() => {
-      req.session.user_id = userData.id;
+      req.session.post_id = postData.id;
       req.session.logged_in = true;
 
-      res.status(200).json(userData);
+      res.status(200).json(postData);
     });
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-router.post('/login', async (req, res) => {
+// Update a post  - Data is in the req.body and req.session
+router.put("/:id", async (req, res) => {
+  // Update a post by its `id` value
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
-
-    if (!userData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
-    }
-
-    const validPassword = await userData.checkPassword(req.body.password);
-
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
-    }
-
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-      
-      res.json({ user: userData, message: 'You are now logged in!' });
+    const post = await Post.update(req.body, {
+      where: {
+        id: req.params.id
+      },
     });
-
-  } catch (err) {
-    res.status(400).json(err);
+    if (!post) {
+      res.status(404).json({
+        message: "Post id not found!"
+      });
+      return;
+    }
+    res.status(200).json(post);
+  } catch (err0r) {
+    res.status(500).json(error);
   }
 });
 
-router.post('/logout', (req, res) => {
-  if (req.session.logged_in) {
-    req.session.destroy(() => {
-      res.status(204).end();
+// Delete a post - Data is in the req.body and req.session
+router.delete("/:id", async (req, res) => {
+  // Delete a post by its `id` value
+  try {
+    const post = await Post.destroy({
+      where: {
+        id: req.params.id
+      }
     });
-  } else {
-    res.status(404).end();
+    if (!post) {
+      res.status(404).json({
+        message: "Post id not found!"
+      });
+      return;
+    }
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
 
