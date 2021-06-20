47ce75fe141489928d50f2519e47e91e03aa1b6d
JAPinell <japinell@yahoo.com>
2021-06-20 13:51:40 -0500

    Improved the profile management

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,47 +1,29 @@
-const newFormHandler = async (event) => {
+const profileHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#project-name').value.trim();
-  const needed_funding = document.querySelector('#project-funding').value.trim();
-  const description = document.querySelector('#project-desc').value.trim();
+  const id = document.querySelector("#id").value.trim();
+  const name = document.querySelector("#username").value.trim();
+  const email = document.querySelector("#email").value.trim();
+  const password = document.querySelector("#password").value.trim();
 
-  if (name && needed_funding && description) {
-    const response = await fetch(`/api/projects`, {
-      method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
+  if (id && name && email && password) {
+    const res = await fetch(`/api/users/${id}`, {
+      method: "PUT",
+      body: JSON.stringify({ id, name, email, password }),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     });
 
-    if (response.ok) {
-      document.location.replace('/profile');
+    if (res.ok) {
+      // If successful, redirect the browser to the homepage
+      document.location.replace("/");
     } else {
-      alert('Failed to create project');
-    }
-  }
-};
-
-const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
-
-    const response = await fetch(`/api/projects/${id}`, {
-      method: 'DELETE',
-    });
-
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert('Failed to delete project');
+      alert("Failed to update profile");
     }
   }
 };
 
 document
-  .querySelector('.new-project-form')
-  .addEventListener('submit', newFormHandler);
-
-document
-  .querySelector('.project-list')
-  .addEventListener('click', delButtonHandler);
+  .querySelector(".profile-form")
+  .addEventListener("submit", profileHandler);
