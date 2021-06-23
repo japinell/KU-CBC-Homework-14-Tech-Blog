5d5d6e6d37622f482de070f777080624959b2e86
JAPinell <japinell@yahoo.com>
2021-06-22 23:37:48 -0500

    Improved forms to handle viewing, adding, editing, deleting, and commenting posts

--- public/js/posts.js
+++ public/js/posts.js
@@ -1,35 +1,35 @@
 const postSubmitHandler = async (event) => {
   event.preventDefault();
 
-  const postId = document.querySelector("#postId").value.trim();
-  const title = document.querySelector("#title").value.trim();
-  const contents = document.querySelector("#contents").value.trim();
+  const postId = document.querySelector('#postId').value.trim();
+  const title = document.querySelector('#title').value.trim();
+  const contents = document.querySelector('#contents').value.trim();
   if (postId && title && contents) {
     const res = await fetch(`/api/comments`, {
-      method: "POST",
+      method: 'POST',
       body: JSON.stringify({ postId, title, contents }),
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
     });
-    console.log(res.ok)
+    console.log(res.ok);
     if (res.ok) {
       // If successful, redirect the browser to the homepage
-      document.location.replace("/");
+      document.location.replace('/');
     } else {
-      alert("Failed to post comment");
+      alert('Failed to post comment');
     }
   }
 };
 
 const postCancelHandler = async (event) => {
   event.preventDefault();
-  window.location.href="/login";
+  window.location.href = '/login';
 };
 
 document
-  .querySelector(".post-form-submit")
-  .addEventListener("submit", postSubmitHandler);
+  .querySelector('.post-form-submit')
+  .addEventListener('submit', postSubmitHandler);
 document
-  .querySelector(".post-form-cancel")
-  .addEventListener("submit", postCancelHandler);
+  .querySelector('.post-form-cancel')
+  .addEventListener('submit', postCancelHandler);
